4ddb9ab64c167d9311db8600503812e507dbc904
romario102040 <romaryjane@hotmail.com>
2022-11-08 17:49:34 -0300

    pessoa service com get

--- src/app/pessoas/pessoa.service.ts
+++ src/app/pessoas/pessoa.service.ts
@@ -1,12 +1,22 @@
 import { DatePipe } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+export class PessoaFiltro {
+  nome?: string;
+  pagina = 0;
+  intensPorPagina = 5;
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PessoaService {
 
+  confirm(arg0: { message: string; accept: () => void; }) {
+    throw new Error('Method not implemented.');
+  }
 
   pessoasUrl = 'http://localhost:8080/pessoas';
 
@@ -15,7 +25,28 @@ export class PessoaService {
 
   ) { }
 
+  pesquisar(filtro: PessoaFiltro): Promise<any> {
+    let params = new HttpParams();
 
+    params = params.set('page', filtro.pagina);
+    params = params.set('size', filtro.intensPorPagina);
+
+    if (filtro.nome) {
+      params = params.set('nome', filtro.nome);
+    }
+
+    return this.http.get(`${this.pessoasUrl}?resumo`, {  params })
+      .toPromise()
+      .then((response: any) => {
+        const pessoas = response['content'];
+        console.log(pessoas)
+        const resultado = {
+          pessoas,
+          total: response['totalElements']
+        };
+        return resultado;
+      });
+}
 
 
   listarPessoa(): Promise<any> {
--- src/app/pessoas/pessoas-pesquisar/pessoas-pesquisar.component.ts
+++ src/app/pessoas/pessoas-pesquisar/pessoas-pesquisar.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { PessoaService } from './../pessoa.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PessoaFiltro } from '../pessoa.service';
+import { ConfirmationService, LazyLoadEvent, MessageService } from 'primeng/api';
+import { ErroHandlerService } from 'src/app/core/erro-handler.service';
+import { Title } from '@angular/platform-browser';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-pessoas-pesquisar',
@@ -7,15 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PessoasPesquisarComponent implements OnInit {
 
+  pessoas = [];
+  filtro = new PessoaFiltro();
+  totalRegistro = 0;
 
-  pessoas = [
-    {nome: 'Romario Ribeiro', cargo: 'Auxiliar de Produção', ativo: true},
-    {nome: 'João das Couves', cargo: 'Fechador de Caixas', ativo: true}
-  ]
 
-  constructor() { }
+  @ViewChild('tabela') grid!: Table;
+
+
+  constructor(private pessoaService: PessoaService,
+    private messagemService: MessageService,
+    private erroHandler: ErroHandlerService,
+    private confirmation: ConfirmationService,
+    private title: Title) { }
 
   ngOnInit(): void {
+    this.title.setTitle('Pessoa Pesquisa')
   }
+
+  pesquisar(pagina = 0): void {
+    this.filtro.pagina = pagina;
+
+    this.pessoaService.pesquisar(this.filtro)
+      .then((resultado: any) => {
+        this.totalRegistro = resultado.total
+        this.pessoas = resultado.pessoas;
+      });
+    }
+
+
+
+    oMudarPagina(event: LazyLoadEvent) {
+      const pagina = event!.first! / event!.rows!;
+      this.pesquisar(pagina);
+    }
 
 }
